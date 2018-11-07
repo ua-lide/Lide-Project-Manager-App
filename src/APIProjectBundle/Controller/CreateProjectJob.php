bb1c379f02c109bb73b16a39d8f10f3aa16571db
Romane Poirier <romanepoirier0@gmail.com>
2018-11-07 22:23:31 +0100

    Update createProject method

--- src/APIProjectBundle/Controller/CreateProjectJob.php
+++ src/APIProjectBundle/Controller/CreateProjectJob.php
@@ -6,26 +6,15 @@ namespace APIProjectBundle\Controller;
 class CreateProjectJob
 {
 
-    private $name;
-    private $environnement_id;
-    private $is_public;
+    private $projet;
 
     /**
      * CreateProjectJob constructor.
-     * @param $name
-     * @param $environnement_id
-     * @param $is_public
+     * @param $projet
      */
-    public function __construct($name, $environnement_id, $is_public)
+    public function __construct($projet)
     {
-        $this->name = $name;
-        $this->environnement_id = $environnement_id;
-        if ($this->is_public == null) {
-            $this->is_public = false;
-        }
-        else {
-            $this->is_public = $is_public;
-        }
+        $this->projet = $projet;
     }
 
 
