876d6a998f0946301671a19b3fddbf25f3ea55b3
Romane Poirier <romanepoirier0@gmail.com>
2018-11-07 23:31:42 +0100

    Add getProjects method

--- src/APIProjectBundle/Controller/ProjectController.php
+++ src/APIProjectBundle/Controller/ProjectController.php
@@ -4,11 +4,14 @@ namespace APIProjectBundle\Controller;
 
 
 use APIProjectBundle\Entity\Projet;
+use FOS\RestBundle\Request\ParamFetcherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use APIProjectBundle\Controller\CreateProjectJob;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class ProjectController extends Controller
 {
@@ -40,7 +43,7 @@ class ProjectController extends Controller
      */
     public function getProject(Request $request) {
         $projet = $this->getDoctrine()->getRepository('APIProjectBundle:Projet')
-            ->find($request->get('id'));
+            ->find($request->get('idProject'));
 
         if (empty($projet)) {
             return new JsonResponse(['message' => 'Project not found'], Response::HTTP_NOT_FOUND);
@@ -50,10 +53,20 @@ class ProjectController extends Controller
     }
 
     /**
-     * @Rest\Get("/api/projects?query")
+     * @Rest\Get("/api/projects")
+     * @Rest\View()
+     * @Rest\QueryParam(name="name")
+     * @Rest\QueryParam(name="user")
+     * @Rest\QueryParam(name="page")
      */
-    public function getProjects() {
+    public function getProjects(ParamFetcherInterface $paramFetcher) {
+        $projects = $this->getDoctrine()->getRepository('APIProjectBundle:Project')
+            ->findBy(
+                array('name'=>$paramFetcher->get('name')),
+                array('user_id'=>$paramFetcher->get('user'))
+            );
 
+        return $projects;
     }
 
     /**
