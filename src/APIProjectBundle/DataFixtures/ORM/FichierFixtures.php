9820172f61f91d6873564fe19d397a8c5143ec61
Romane Poirier <romanepoirier0@gmail.com>
2018-11-18 19:56:28 +0100

    Update Fichier and Projet fixtures

--- src/APIProjectBundle/DataFixtures/ORM/FichierFixtures.php
+++ src/APIProjectBundle/DataFixtures/ORM/FichierFixtures.php
@@ -4,34 +4,32 @@ namespace APIProjectBundle\DataFixtures\ORM;
 
 use APIProjectBundle\Entity\Fichier;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class FichierFixtures extends Fixture {
+class FichierFixtures extends Fixture implements DependentFixtureInterface {
 
     public function load(ObjectManager $manager) {
 
         $fichier1 = new Fichier();
 
-        $fichier1->setName("fichier1");
+        $fichier1->setFileName("fichier1");
         $fichier1->setPath("/");
-        $fichier1->setCreatedAt(date_create_from_format(DATE_ISO8601, strtotime('2018-10-17 15:01:00')));
-        $fichier1->setUpdatedAt(date_create_from_format(DATE_ISO8601, strtotime('2018-10-17 15:01:00')));
+        $fichier1->setProject($this->getReference('projet1'));
         $manager->persist($fichier1);
 
         $fichier2 = new Fichier();
 
-        $fichier2->setName("fichier2");
+        $fichier2->setFileName("fichier2");
         $fichier2->setPath("/");
-        $fichier2->setCreatedAt(date_create_from_format(DATE_ISO8601, strtotime('2018-10-17 15:01:00')));
-        $fichier2->setUpdatedAt(date_create_from_format(DATE_ISO8601, strtotime('2018-10-17 15:01:00')));
+        $fichier1->setProject($this->getReference('projet1'));
         $manager->persist($fichier2);
 
         $fichier3 = new Fichier();
 
-        $fichier3->setName("fichier3");
+        $fichier3->setFileName("fichier3");
         $fichier3->setPath("/");
-        $fichier3->setCreatedAt(date_create_from_format(DATE_ISO8601, strtotime('2018-10-17 15:01:00')));
-        $fichier3->setUpdatedAt(date_create_from_format(DATE_ISO8601, strtotime('2018-10-17 15:01:00')));
+        $fichier1->setProject($this->getReference('projet2'));
         $manager->persist($fichier3);
 
 
@@ -39,4 +37,15 @@ class FichierFixtures extends Fixture {
 
     }
 
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    public function getDependencies() {
+        return array(
+            ProjetFixtures::class,
+        );
+    }
 }
--- src/APIProjectBundle/DataFixtures/ORM/ProjetFixtures.php
+++ src/APIProjectBundle/DataFixtures/ORM/ProjetFixtures.php
@@ -12,24 +12,26 @@ class ProjetFixtures extends Fixture {
 
         $projet1 = new Projet();
 
-        $projet1->setName("projet1");
+        $projet1->setProjectName("projet1");
         $projet1->setUserId(1);
         $projet1->setEnvironnementId(1);
         $projet1->setIsPublic(true);
         $projet1->setIsArchived(false);
-        $projet1->setCreatedAt(date_create_from_format(DATE_ISO8601, strtotime('2018-10-17 15:01:00')));
-        $projet1->setUpdatedAt(date_create_from_format(DATE_ISO8601, strtotime('2018-10-17 15:01:00')));
+
+        $this->addReference('projet1', $projet1);
+
         $manager->persist($projet1);
 
         $projet2 = new Projet();
 
-        $projet2->setName("projet2");
+        $projet2->setProjectName("projet2");
         $projet2->setUserId(2);
         $projet2->setEnvironnementId(2);
         $projet2->setIsPublic(true);
         $projet2->setIsArchived(true);
-        $projet2->setCreatedAt(date_create_from_format(DATE_ISO8601, strtotime('2018-10-15 15:01:00')));
-        $projet2->setUpdatedAt(date_create_from_format(DATE_ISO8601, strtotime('2018-10-17 15:21:00')));
+
+        $this->addReference('projet2', $projet2);
+
         $manager->persist($projet2);
 
 
