c20897babc8119720e5967a7a9c4e9a658308e22
Romane Poirier <romanepoirier0@gmail.com>
2018-11-21 17:32:31 +0100

    Add getFilesAction method and tests + update getFileAction method

--- tests/APIProjectBundle/Controller/FichierControllerTest.php
+++ tests/APIProjectBundle/Controller/FichierControllerTest.php
@@ -9,7 +9,7 @@ class FichierControllerTest extends WebTestCase {
 
     public function testGetExistingFile() {
         $client = static::createClient();
-        $client->request('GET', '/api/project/1/files/4');
+        $client->request('GET', '/api/project/2/files/4');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
@@ -20,6 +20,21 @@ class FichierControllerTest extends WebTestCase {
 
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
         $this->assertContains('File not found', $client->getResponse()->getContent());
+    }
+
+    public function testGetExistingFileWithWrongProject() {
+        $client = static::createClient();
+        $client->request('GET', '/api/project/1/files/4');
+
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+        $this->assertContains('appartient pas au projet specifie', $client->getResponse()->getContent());
+    }
+
+    public function testGetFiles() {
+        $client = static::createClient();
+        $client->request('GET', '/api/project/2/files');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
 }
