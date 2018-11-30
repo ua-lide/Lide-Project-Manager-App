a5d213de6452cbf545b2f63e1bb00476b6fb906e
pviolette-fr <violette.paulin@gmail.fr>
2018-11-30 15:09:53 +0100

    Fixed tests : use an authenticated client for all requests

--- tests/APIProjectBundle/Controller/FichierControllerTest.php
+++ tests/APIProjectBundle/Controller/FichierControllerTest.php
@@ -7,15 +7,17 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class FichierControllerTest extends WebTestCase {
 
+    use JWTAuthenticationClient;
+
     public function testGetExistingFile() {
-        $client = static::createClient();
+        $client = static::createAuthentificatedAdminClient();
         $client->request('GET', '/api/project/2/files/4');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
     public function testGetNonExistingFile() {
-        $client = static::createClient();
+        $client = static::createAuthentificatedAdminClient();
         $client->request('GET', '/api/project/1/files/6546');
 
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
@@ -23,7 +25,7 @@ class FichierControllerTest extends WebTestCase {
     }
 
     public function testGetExistingFileWithWrongProject() {
-        $client = static::createClient();
+        $client = static::createAuthentificatedAdminClient();
         $client->request('GET', '/api/project/1/files/4');
 
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
@@ -31,7 +33,7 @@ class FichierControllerTest extends WebTestCase {
     }
 
     public function testGetFiles() {
-        $client = static::createClient();
+        $client = static::createAuthentificatedAdminClient();
         $client->request('GET', '/api/project/2/files');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
--- tests/APIProjectBundle/Controller/ProjectControllerTest.php
+++ tests/APIProjectBundle/Controller/ProjectControllerTest.php
@@ -3,36 +3,23 @@
 namespace Tests\APIProjectBundle\Controller;
 
 
-use AppBundle\Security\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProjectControllerTest extends WebTestCase {
 
-
-    public static function createAuthentificatedAdminClient(){
-
-        $user = new User(1, 'admin', ['ROLE_ADMIN', 'ROLE_USER']);
-
-        $client = static::createClient();
-        $jwtAuth = $client->getContainer()->get('lexik_jwt_authentication.jwt_manager');
-        $token = $jwtAuth->create($user);
-
-        $client = static::createClient();
-        $client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $token));
-
-        return $client;
-    }
+    use JWTAuthenticationClient;
 
     public function testGetExistingProject() {
         $client = static::createAuthentificatedAdminClient();
         $client->request('GET', '/api/project/1');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-
+        dump($client->getResponse()->getContent());
     }
 
     public function testGetNonExistingProject() {
-        $client = static::createClient();
+        $x = static::createClient();
+        $client = static::createAuthentificatedAdminClient();
         $client->request('GET', '/api/project/1564');
 
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
@@ -40,28 +27,28 @@ class ProjectControllerTest extends WebTestCase {
     }
 
     public function testGetProjects() {
-        $client = static::createClient();
+        $client = static::createAuthentificatedAdminClient();
         $client->request('GET', '/api/projects');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
     public function testGetProjectsByUserAndName() {
-        $client = static::createClient();
-        $client->request('GET', '/api/projects?name=projet1&user_id=1');
+        $client = static::createAuthentificatedAdminClient();
+        $client->request('GET', '/api/projects?name=newname&user_id=1');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
     public function testGetProjectsByUser() {
-        $client = static::createClient();
+        $client = static::createAuthentificatedAdminClient();
         $client->request('GET', '/api/projects?user_id=1');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
     public function testGetProjectsByNonExistingUser() {
-        $client = static::createClient();
+        $client = static::createAuthentificatedAdminClient();
         $client->request('GET', '/api/projects?user_id=2671');
 
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
@@ -69,14 +56,14 @@ class ProjectControllerTest extends WebTestCase {
     }
 
     public function testGetProjectsByName() {
-        $client = static::createClient();
-        $client->request('GET', '/api/projects?name=projet1');
+        $client = static::createAuthentificatedAdminClient();
+        $client->request('GET', '/api/projects?name=newname');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
     public function testGetProjectsByNonExistingName() {
-        $client = static::createClient();
+        $client = static::createAuthentificatedAdminClient();
         $client->request('GET', '/api/projects?name=WrongName');
 
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
@@ -84,7 +71,7 @@ class ProjectControllerTest extends WebTestCase {
     }
 
     public function testSetProject() {
-        $client = static::createClient();
+        $client = static::createAuthentificatedAdminClient();
         $client->request('PUT',
             '/api/project/1',
             array(),
